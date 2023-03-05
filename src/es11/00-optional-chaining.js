6e3690096dc7fd12669fea5bf7650502d00ab88b
Abraham Fortunatte <a.fortunatte@gmail.com>
2023-03-05 05:44:12 -0300

    optional chaining, bigint, nullish

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    afort: {
+        country: "CL"
+    },
+    ana: {
+        country: "CO"
+    }
+}
+
+console.log(users?.bebeloper?.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber=12312312342353465741n;
+const anotherBigNumber=BigInt(12312312342353465741);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
